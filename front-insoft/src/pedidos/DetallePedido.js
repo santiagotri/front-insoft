bdada9b0f03b6f488e455b8bc73de472f88d9872
Santiago Triana <a.trianaa@uniandes.edu.co>
2022-10-10 19:25:38 -0500

    Retoques en interfaz de Pedidos evita botón cancelar cuando cancelado

--- front-insoft/src/pedidos/DetallePedido.js
+++ front-insoft/src/pedidos/DetallePedido.js
@@ -3,28 +3,31 @@ import "./Pedidos.css";
 import { useEffect, useState } from "react";
 import EditarPedidoForm from "./EditarPedidoForm";
 
-function DetallePedido(props){
-    
-    return (
-        <div className="col">
-            <Card className="mx-auto" style={{marginTop:"2rem", width:"17rem"}}>
-              <Card.Title style={{ textAlign: "center" , marginTop:"1rem", color:"#e2322c"}}>
-                Pedido #{props.id}
-              </Card.Title>
-              <Card.Body>
-                <h6>
-                    Cliente
-                </h6>
-                <p>{props.cliente}</p>
-                <h6>
-                    Productos
-                </h6>
-                <p>{props.productos}</p>
-              </Card.Body>
-              <EditarPedidoForm id={props.id}></EditarPedidoForm>
-            </Card>
-        </div>
-      );
+function DetallePedido(props) {
+  function editarPedido() {
+    if (props.estado !== "Cancelado" && props.estado !== "Finalizado")
+      return <EditarPedidoForm id={props.id}></EditarPedidoForm>;
+  }
+  return (
+    <div className="col">
+      <Card className="mx-auto" style={{ marginTop: "2rem", width: "17rem" }}>
+        <Card.Title
+          style={{ textAlign: "center", marginTop: "1rem", color: "#e2322c" }}
+        >
+          Pedido #{props.id}
+        </Card.Title>
+        <Card.Body>
+          <h6>Cliente</h6>
+          <p>{props.cliente}</p>
+          <h6>Productos</h6>
+          <p>{props.productos}</p>
+          <h6>Estado</h6>
+          <p>{props.estado}</p>
+        </Card.Body>
+        {editarPedido()}
+      </Card>
+    </div>
+  );
 }
 
 export default DetallePedido;
--- front-insoft/src/pedidos/ListaPedidos.js
+++ front-insoft/src/pedidos/ListaPedidos.js
@@ -6,14 +6,14 @@ function ListaPedidos(){
     <div>
 
         <div className="row">
-            <DetallePedido id="1232" cliente="Mc Donalds" productos="1 Mouse, 2 Teclados"></DetallePedido>
-            <DetallePedido id="42" cliente="Farmatodo" productos="1 Mouse, 2 Sillas, 1 Audifonos"></DetallePedido>
-            <DetallePedido id="312" cliente="Mc Donalds" productos="1 Silla, 2 Teclados"></DetallePedido>
-            <DetallePedido id="141" cliente="Carulla" productos="1 Mouse, 2 Escritorios"></DetallePedido>
-            <DetallePedido id="76" cliente="Carulla" productos="1 Audiofonos, 2 Teclados"></DetallePedido>
-            <DetallePedido id="7255" cliente="Mc Donalds" productos="1 Mouse"></DetallePedido>
-            <DetallePedido id="3" cliente="Farmatodo" productos="1 Silla, 2 Teclados"></DetallePedido>
-            <DetallePedido id="345" cliente="Exito" productos="1 Teclado"></DetallePedido>
+            <DetallePedido id="1232" cliente="Mc Donalds" productos="1 Mouse, 2 Teclados" estado="En proceso"></DetallePedido>
+            <DetallePedido id="42" cliente="Farmatodo" productos="1 Mouse, 2 Sillas, 1 Audifonos" estado="Esperando pago"></DetallePedido>
+            <DetallePedido id="312" cliente="Mc Donalds" productos="1 Silla, 2 Teclados" estado="En proceso"></DetallePedido>
+            <DetallePedido id="141" cliente="Carulla" productos="1 Mouse, 2 Escritorios" estado="En proceso"></DetallePedido>
+            <DetallePedido id="76" cliente="Carulla" productos="1 Audiofonos, 2 Teclados" estado="Cancelado"></DetallePedido>
+            <DetallePedido id="7255" cliente="Mc Donalds" productos="1 Mouse" estado="Finalizado"></DetallePedido>
+            <DetallePedido id="3" cliente="Farmatodo" productos="1 Silla, 2 Teclados" estado="Cancelado"></DetallePedido>
+            <DetallePedido id="345" cliente="Exito" productos="1 Teclado" estado="En proceso"></DetallePedido>
         </div>
     </div>
     );
